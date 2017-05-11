927cc9b52e04cceffa6a83c97538042dbe5336bd
Guilherme Henrique Melo <guilherme.melo0500@gmail.com>
2017-05-11 07:28:45 -0300

    Refeito

--- produtor_consumidor_DEB.c
+++ produtor_consumidor_DEB.c
@@ -5,6 +5,7 @@
 pthread_cond_t buffer_cheio = PTHREAD_COND_INITIALIZER;
 pthread_cond_t buffer_vazio = PTHREAD_COND_INITIALIZER;
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; 
 
 #define TOTAL 50
 int posicoes_ocupadas=0;
@@ -13,27 +14,29 @@ int valor=0;
 
 int buffer[TOTAL];
 
-pthread_mutex_t mutexP;
-pthread_mutex_t mutexC;
+
 
 
 void *produtor(void *argumentos){
 	
 	while (1){
+		pthread_mutex_lock(&mutex);
+
 		if (posicoes_ocupadas == TOTAL) {
 			printf("!!! Produtor Esperando   !!!\n");
-			pthread_cond_wait(&buffer_cheio, &mutexP); // Estou tentando com esses mutex's! Caso não dê certo, utilizar os mutex's que a professora criou!!!
-		} else {
-			pthread_mutex_lock(&mutexP);
-		 
-			buffer[posicoes_ocupadas] = valor;
-			printf("Valor produzido: %d\n", buffer[posicoes_ocupadas]);
-			valor++;
-			posicoes_ocupadas++;
-		
-			pthread_mutex_unlock(&mutexP);
+			pthread_cond_wait(&buffer_cheio, &mutex); // Estou tentando com esses mutex's! Caso não dê certo, utilizar os mutex's que a professora criou!!!
+		}
+
+		buffer[posicoes_ocupadas] = valor;
+		printf("Valor produzido: %d\n", buffer[posicoes_ocupadas]);
+		valor++;
+		posicoes_ocupadas++;
+
+		if (posicoes_ocupadas==1){
+			pthread_cond_signal(&buffer_vazio);
 		}
 		
+		pthread_mutex_unlock(&mutex);
 	}
 
 	pthread_exit(NULL);
@@ -44,19 +47,22 @@ void *consumidor(void *argumentos){
 	int leitura;
 
 	while(1){
+		pthread_mutex_lock(&mutex);
+
 		if (posicoes_ocupadas == 0) {
 			printf("!!!  Consumidor Esperando  !!!\n");
-			pthread_cond_wait(&buffer_vazio, &mutexC);
-		} else {
-			pthread_mutex_lock(&mutexC);
+			pthread_cond_wait(&buffer_vazio, &mutex);
+		} 
 
-			leitura = buffer[posicoes_ocupadas];
-			printf("Valor lido: %d\n", leitura);
-			posicoes_ocupadas--;
+		leitura = buffer[posicoes_ocupadas];
+		printf("Valor lido: %d\n", leitura);
+		posicoes_ocupadas--;
 		
-			pthread_mutex_unlock(&mutexC);
+		if (posicoes_ocupadas== TOTAL-1){
+			pthread_cond_signal(&buffer_cheio);
 		}
 
+		pthread_mutex_unlock(&mutex);
 	}
 
 	pthread_exit(NULL);
@@ -68,6 +74,7 @@ pthread_t thread_consumidor;
 
 	pthread_mutex_init(&mutexC, NULL);
 	pthread_mutex_init(&mutexP, NULL);
+	//pthread_mutex_init(&mutex, NULL);
 
 // Criando thread do produtor
 	pthread_create(&thread_produtor, NULL, produtor, NULL);
